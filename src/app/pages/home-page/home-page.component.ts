ad3a83686bbdc198585a0935b69f8e08c4258556
Anandu K U <ananduku12@gmail.com>
2023-11-22 12:06:55 +0530

    ak22 commit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
@@ -14,7 +15,9 @@ export class HomePageComponent {
   inputAdd!: string;
   items: any;
   constructor(private api:ApiService){  }
+
   todos:any='';
+  newTaskTitle: string ='';
   ngOnInit(){
     this.api.getTodo().subscribe((data:any)=>{
       this.todos=data
@@ -22,5 +25,13 @@ export class HomePageComponent {
     }
     delete(id:any){
       this.todos = this.todos.filter((e : {id:any})=> e.id != id)
+    }
+    addNewTask(){
+      if (this.newTaskTitle.trim() !== ''){
+        const newTask = { title: this.newTaskTitle, finished:false};
+        this.todos.push(newTask);
+        this.newTaskTitle= '';//clear the input field after adding task
+      }
+    
     }
 }
